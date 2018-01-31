67646b05313cd35495e292e85dbc93b396f282ca
stygm <stygm@DESKTOP-IMF9U2H>
2018-01-31 10:19:50 -0500

    Ajoute manager et service

---
+++ ListeContact/ContactService/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactService
+{
+    public class Service
+    {
+        //connection a la basse de donnees
+        static string connectionString = @"Data Source=DESKTOP-IMF9U2H;
+                Initial Catalog = listecontact;
+                Integrated Security = True;
+                 Connect Timeout = 5";
+
+    }
+}
