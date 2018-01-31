c03f3e7f8f580d74fabbbb93cdefdc5672b6d7ed
WhereRUBeen <brice.mimifir.a@gmail.com>
2018-01-31 15:25:04 -0500

    Ajout des classes  Genre, Adresse,Categorie

--- Entities/Class1.cs
+++ Entities/Class1.cs
@@ -10,27 +10,44 @@ namespace Entities
     public class Contact
     {
         public int Id { get; set; }
-        public string Nom { get; set; }
-        public string Prenom { get; set; }
-        public string Telephone { get; set; }
-        public string Mail { get; set; }
-        //id_adresse
-        public Categorie categorie_id { get; set; }
+        public Personne Personne { get; set; }
     }
+
+    //class - table personne
+    public class Personne
+    {
+        public int Id_personne { get; set; }
+        public string nom_personne { get; set; }
+        public string prenom_personne { get; set; }
+        public string mail { get; set; }
+        public Adresse id_adresse { get; set; }
+        public Categorie id_categorie { get; set; }
+        public Genre id_genre { get; set; }
+    }
+
     //class - table categorie
     public class Categorie
     {
         public int Id_categorie { get; set; }
-        public string Nom_categori { get; set; }
+        public string Type_categorie{ get; set; }
     }
 
+    //claa - table genre
+    public class Genre
+    {
+        public int id_genre { get; set; }
+        public string type_genre { get; set; }
+
+    }
     //class - table adresse
     public class Adresse
     {
+        
         public int Id_adresse { get; set; }
         public string Numero_rue { get; set; }
         public string Nom_rue { get; set; }
         public string Code_postal { get; set; }
+        public string Ville { get; set; }
         public string Province { get; set; }
     }
 
